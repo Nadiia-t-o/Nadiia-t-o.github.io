a3396e912c3d0387d0723ac9d4cdf3a3c62bae04
Nadiia Tovkailo <Nadiia_Tovkailo@epam.com>
2020-07-31 17:01:38 +0300

    Added autoplay to video bnt

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     const playBtn = document.querySelector ('.video__play-btn');
           overlay = document.querySelector ('.video__overlay');
+          video = document.querySelector ('.video__screen')
 
     playBtn.addEventListener("click", function(){
         overlay.style.display = "none";
+
+        let symbol = video.src.indexOf("?") > -1 ? "&" : "?";
+        
+        video.src += symbol + "autoplay=1";
     });
 });
